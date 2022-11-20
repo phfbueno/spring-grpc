c680545d66d2f54592524c0f7d2a25c95add83d8
Paulo Bueno <paulobueno@pop-os.localdomain>
2022-11-20 17:33:29 -0300

    primeiro hello word em grpc

---
+++ src/main/java/com/github/phfbueno/SpringGrpcCourseApplication.java
@@ -0,0 +1,13 @@
+package com.github.phfbueno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGrpcCourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGrpcCourseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/phfbueno/SpringGrpcCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.phfbueno;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGrpcCourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
